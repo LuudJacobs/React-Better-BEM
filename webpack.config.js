3efe79eb5dce50a38792345f4d0524a2b8e0afee
Luud Jacobs <luud.jacobs@pwc.com>
2019-11-15 15:18:12 +0100

    Changed library target

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve('lib'),
         filename: 'Bem.js',
-        libraryTarget: 'commonjs2'
+        libraryTarget: 'umd'
     },
     module: {
         rules: [
